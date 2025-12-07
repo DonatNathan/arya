2402f1d5f7fb34f2033fb02d6a72148e08b43b23
DonatNathan <nathan.donat-filliod@epitech.eu>
2025-12-07 08:27:57 +0100

    [ADD] - Intent engine that choose right command

--- includes/TranscriptAnalyzer.hpp
+++ includes/TranscriptAnalyzer.hpp
@@ -1,26 +1,17 @@
-#include <fstream>
 #include <string>
 #include <algorithm>
 #include <vector>
-#include <nlohmann/json.hpp>
 
 #pragma once
-
-struct Command {
-    int code;
-    std::string value;
-};
 
 class TranscriptAnalyzer {
 
     private:
         std::string& a_lastTranscript;
-        std::vector<Command> a_commandList;
 
     public:
         TranscriptAnalyzer(std::string& lastTranscript);
-        void loadCommands();
         std::string toLower(const std::string& s);
-        int findCommand();
+        std::string analyzeTranscript();
 
 };
--- src/TranscriptAnalyzer.cpp
+++ src/TranscriptAnalyzer.cpp
@@ -2,26 +2,7 @@
 
 TranscriptAnalyzer::TranscriptAnalyzer(std::string& lastTranscript) : a_lastTranscript(lastTranscript)
 {
-    loadCommands();
-};
-
-void TranscriptAnalyzer::loadCommands()
-{
-    std::string path = "../assets/commands.json";
-    std::ifstream file(path);
-    if (!file.is_open()) {
-        throw std::runtime_error("Cannot open command file: " + path);
-    }
-
-    nlohmann::json j;
-    file >> j;
-
-    for (auto& entry : j) {
-        a_commandList.push_back({
-            entry["code"].get<int>(),
-            entry["value"].get<std::string>()
-        });
-    }
+    
 };
 
 std::string TranscriptAnalyzer::toLower(const std::string& s)
@@ -31,17 +12,9 @@ std::string TranscriptAnalyzer::toLower(const std::string& s)
     return out;
 };
 
-int TranscriptAnalyzer::findCommand()
+std::string TranscriptAnalyzer::analyzeTranscript()
 {
     std::string t = toLower(a_lastTranscript);
 
-    for (const auto& cmd : a_commandList) {
-        std::string valueLower = toLower(cmd.value);
-
-        if (t.find(valueLower) != std::string::npos) {
-            return cmd.code;
-        }
-    }
-
-    return 0;
+    return t;
 };
